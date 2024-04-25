a879bb9c409703d040bde3eead4e71395b8250bf
Vishnu <iam.vishnu.bhaskar@gmail.com>
2024-04-25 10:57:57 +0530

    User Table problem fixed and added jQuery for admin header

---
+++ public/javascript/admin/admin.js
@@ -0,0 +1,20 @@
+$('#user-info-hover').hide()
+$(document).ready(() => {
+    // Admin Header user-info hover effect
+    $('#admin-user-info').hover(() => {
+        $('#user-info-hover').show()
+    }, () => {
+        $('#user-info-hover').hide()
+    })
+
+    $('#user-info-hover').hover(() => {
+        $('#user-info-hover').show()
+    }, () => {
+        $('#user-info-hover').hide()
+    })
+
+    const admin_menu_toggle = $('#admin-menu-toggle')
+    const admin_sidebar = $('#admin-sidebar')
+
+    
+})
